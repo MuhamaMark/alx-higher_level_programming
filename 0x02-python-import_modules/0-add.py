c4c9a9e6c217037bb647a0e1eb966bba73c1d3b7
muhama mark <muhamamark@gmail.com>
2022-09-23 05:18:34 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if _name_ == "_main_"
-  from add_0 import add
-  a = 1
-  b = 2
-  print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
