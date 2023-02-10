69a307d36a8aadbcd6ea2e2e3fd783c3aaee19e0
muhama mark <muhamamark@gmail.com>
2023-02-10 23:32:28 +0300

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 
 const fs = require('fs');
 const filename = process.argv[2];
-fs.readFile(filename, 'utf-8', (error, content) => {
+fs.readFile( filename, 'utf-8', (error, content ) => {
   if (error) {
-    console.log(error);
+    console.log( error );
   } else {
     console.log(content);
   }
